9827daf0b191ef1e14acfa1be5b6450d2bde680d
Yonghao Jin <sljinyonghao@hotmail.com>
2020-09-29 10:35:12 +0800

    fix page path

---
+++ docs/build/precache-manifest.73c0e1a7d2ed7d37eab5baab37d572ea.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd1ef9433206d187f45fc58751a7a0d3",
+    "url": "/vocabulary/index.html"
+  },
+  {
+    "revision": "89a08197a518f5f90841",
+    "url": "/vocabulary/static/css/2.4287e30b.chunk.css"
+  },
+  {
+    "revision": "acfc837e7b32953c230f",
+    "url": "/vocabulary/static/css/main.b08e4669.chunk.css"
+  },
+  {
+    "revision": "89a08197a518f5f90841",
+    "url": "/vocabulary/static/js/2.55b8f5d9.chunk.js"
+  },
+  {
+    "revision": "849657740f66d00720db130cc1e94c97",
+    "url": "/vocabulary/static/js/2.55b8f5d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "acfc837e7b32953c230f",
+    "url": "/vocabulary/static/js/main.212cb1d0.chunk.js"
+  },
+  {
+    "revision": "10a18afd8848cc903e0d",
+    "url": "/vocabulary/static/js/runtime-main.f525e0cf.js"
+  },
+  {
+    "revision": "085b1dd8427dbeff10bd55410915a3f6",
+    "url": "/vocabulary/static/media/fa-brands-400.085b1dd8.ttf"
+  },
+  {
+    "revision": "0fabb6606be4c45acfeedd115d0caca4",
+    "url": "/vocabulary/static/media/fa-brands-400.0fabb660.eot"
+  },
+  {
+    "revision": "cac68c831145804808381a7032fdc7c2",
+    "url": "/vocabulary/static/media/fa-brands-400.cac68c83.woff2"
+  },
+  {
+    "revision": "ccfdb9dc442be0c629d331e94497428b",
+    "url": "/vocabulary/static/media/fa-brands-400.ccfdb9dc.svg"
+  },
+  {
+    "revision": "dc0bd022735ed218df547742a1b2f172",
+    "url": "/vocabulary/static/media/fa-brands-400.dc0bd022.woff"
+  },
+  {
+    "revision": "05b53beb21e3ef13d28244545977152d",
+    "url": "/vocabulary/static/media/fa-regular-400.05b53beb.woff"
+  },
+  {
+    "revision": "1a78af4105d4d56e6c34f76dc70bf1bc",
+    "url": "/vocabulary/static/media/fa-regular-400.1a78af41.ttf"
+  },
+  {
+    "revision": "3a3398a6ef60fc64eacf45665958342e",
+    "url": "/vocabulary/static/media/fa-regular-400.3a3398a6.woff2"
+  },
+  {
+    "revision": "ad3a7c0d77e09602f4ab73db3660ffd8",
+    "url": "/vocabulary/static/media/fa-regular-400.ad3a7c0d.eot"
+  },
+  {
+    "revision": "e75dfd904d366a2560c63c23cfc98ef8",
+    "url": "/vocabulary/static/media/fa-regular-400.e75dfd90.svg"
+  },
+  {
+    "revision": "03ba7cb710104df27f1c9c46d64bee4e",
+    "url": "/vocabulary/static/media/fa-solid-900.03ba7cb7.svg"
+  },
+  {
+    "revision": "781e85bb50c8e8301c30de56b31b1f04",
+    "url": "/vocabulary/static/media/fa-solid-900.781e85bb.ttf"
+  },
+  {
+    "revision": "89bd2e38475e441a5cd70f663f921d61",
+    "url": "/vocabulary/static/media/fa-solid-900.89bd2e38.eot"
+  },
+  {
+    "revision": "c500da19d776384ba69573ae6fe274e7",
+    "url": "/vocabulary/static/media/fa-solid-900.c500da19.woff2"
+  },
+  {
+    "revision": "ee09ad7553b8ad3d81150d609d5341a0",
+    "url": "/vocabulary/static/media/fa-solid-900.ee09ad75.woff"
+  }
+]);
